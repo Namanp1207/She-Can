934b2ef845af59fe25e497fa79c498528f90fcf1
Naman Parmar <150473389+Namanp1207@users.noreply.github.com>
2026-05-30 10:42:10 +0530

    Update App.jsx

--- shecan/src/App.jsx
+++ shecan/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div className='app'>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/form' element={<Form/>} />
+        <Route path='/form' element={<Form url={url} />} />
       </Routes>
     </div>
   )

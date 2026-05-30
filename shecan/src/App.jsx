30befa2b7ee1a8df8b7a25bbab473e48142de16f
Naman Parmar <150473389+Namanp1207@users.noreply.github.com>
2026-05-30 10:36:50 +0530

    Fix import path and route case for Form component

--- shecan/src/App.jsx
+++ shecan/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Form from './pages/Form/Form.jsx'
+import Form from './pages/Form/Form'
 
 const App = () => {
 
@@ -11,7 +11,7 @@ const App = () => {
     <div className='app'>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/Form' element={<Form/>} />
+        <Route path='/form' element={<Form/>} />
       </Routes>
     </div>
   )

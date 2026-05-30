910a9cfcd30ef26c300345dd5db8648c7748311d
Naman Parmar <150473389+Namanp1207@users.noreply.github.com>
2026-05-30 10:06:00 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Dashboard from './pages/dashboard/Dashboard.jsx'
+import Dashboard from './pages/Dashboard/Dashboard.jsx'
 
 const App = () => {
 
